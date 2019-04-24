11a05d8bf43bc24c905e1fe6963b1fce75d4e0c9
luis_prado <luis_prado@hst.com.br>
2019-04-24 11:37:29 -0300

    TDD

--- seleniumWebDriver/actions/PesquisarGoogle.java
+++ seleniumWebDriver/actions/PesquisarGoogle.java
@@ -1,5 +1,55 @@
 package actions;
 
-public class PesquisarGoogle {
+import org.apache.xml.serializer.utils.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import seleniumWebDriver.commons.Classes;
+
+public class PesquisarGoogle {
+	Classes classes = new Classes();
+	Utils utils;
+
+	@BeforeMethod
+	// BeforeMethod executa sempre antes de todo @Test
+	public void abrirNavegador() {
+
+		classes.abrirNavegador.abrirNavegador();
+
+	}
+
+	@Test
+	// é a esecução de cada teste
+	public void digitarCampoDePesquisa() {
+
+		WebElement inputPesquisa = classes.utils.getDriver().findElement(By.id("lst-ib"));
+		inputPesquisa.sendKeys("automacao de testes");
+	}
+
+	@Test
+	public void clicarBotaoPesquisar() {
+		digitarCampoDePesquisa();
+
+		WebElement botaoPesquisar = classes.utils.getDriver()
+				.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[2]/div[3]/center[1]/input[1]"));
+
+		botaoPesquisar.click();
+	}
+
+	@AfterMethod
+	// AfterMethod executa sempre antes de todo @Test
+	public void fecharNavegador() {
+		classes.fecharNavegador.fecharNavegador();
+	}
+
+	public Utils getUtils() {
+		return utils;
+	}
+
+	public void setUtils(Utils utils) {
+		this.utils = utils;
+	}
 }
